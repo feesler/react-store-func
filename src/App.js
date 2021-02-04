3161e03b8b4b23a8eb75796c57a0c0ed10b6b930
Ilya Makarov <feesler@gmail.com>
2021-02-04 07:29:32 +0300

    * Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { ShopItem } from './models/ShopItem';
+import { ShopItemFunc } from './components/ShopItemFunc';
+
+function App() {
+  const item = new ShopItem({
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  });
+
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,27 @@
+import { PropTypes } from 'prop-types';
+import { ShopItem } from '../models/ShopItem';
+
+export function ShopItemFunc(props) {
+  const { item } = props;
+
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">
+        {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.formatPrice()}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(ShopItem)
+};
